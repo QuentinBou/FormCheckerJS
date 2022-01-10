17d4cc070dcf865e94f05e5fc6d1429d03860566
Quentin Bourgeois <quentintgc@live.fr>
2022-01-10 18:11:12 +0100

    update error msg

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const passwordError = [
   "Votre mot de passe ne correspond pas.",
 ];
 
+const pseudoInp = document.querySelector('#pseudo')
+const emailInp = document.querySelector('#mail')
+const passwordInp = document.querySelector('#password')
+const confirmInp = document.querySelector('#checkpassword')
+
 // Les selecteurs
 const inputs = document.querySelectorAll(
   'input[type="text"], input[type="password"]'
@@ -21,123 +26,123 @@ const passwordCont = document.querySelector(".checkpassword-container");
 const form = document.querySelector("form");
 // LOGIQUE
 
+
+
+// Création d'érreur box
+function createError(msg){
+  Swal.fire({
+    toast: true,
+    position : 'top-end',
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    icon: 'error',
+    title: msg
+  })
+}
+
+// Création Success box
+function createSuccess(){
+  Swal.fire({
+    position: 'center',
+    icon: 'success',
+    title: 'Inscription confirmée.',
+    text: 'Bienvenue parmis nous !',
+    confirmButtonText: 'Merci.',
+  })
+}
+
 // Fonctions
 
-const errorDisplay = (tag, msg, valid) => {
-  const container = document.querySelector(`.${tag}-container`);
-  const span = document.querySelector(`.${tag}-container > span`);
-  const inputBorder = document.querySelector(`.${tag}-container > input`);
-
-  if (!valid) {
-    container.classList.add("error");
-    inputBorder.classList.add("error-input");
-    span.textContent = msg;
-  } else {
-    container.classList.remove("error");
-    inputBorder.classList.remove("error-input");
-  }
-};
-
-const pseudoCheck = (value) => {
+const pseudoCheck = () => {
+  let value = pseudoInp.value
   if (value.length > 0 && (value.length < 5 || value.length > 24)) {
-    errorDisplay("pseudo", pseudoError[0]);
+    createError(pseudoError[0]);
     pseudo = null;
+    return false
   } else if (!value.match(/^[a-zA-Z0-9_.-]*$/)) {
-    errorDisplay("pseudo", pseudoError[1]);
+    createError(pseudoError[1]);
     pseudo = null;
+    return false
   } else {
-    errorDisplay("pseudo", "", true);
-    pseudo = value;
+    pseudo = value
+    return true
   }
 };
 
-const emailCheck = (value) => {
+const emailCheck = () => {
+  let value = emailInp.value
   if (!value.match(/^[\w_-]+@[\w-]+\.[a-z]{2,4}$/i)) {
-    errorDisplay("mail", emailError);
+    createError(emailError);
     mail = null;
+    return false
   } else {
-    errorDisplay("mail", "", true);
     mail = value;
+    return true
   }
 };
 
-const passwordCheck = (value) => {
+const passwordCheck = () => {
+  let value = passwordInp.value
   if (!value.match(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/)) {
-    errorDisplay("password", passwordError[0]);
-    passwordCont.style.marginTop = "30px";
+    createError(passwordError[0]);
     password = null;
+    return false
   } else {
-    errorDisplay("password", "", true);
-    passwordCont.style.marginTop = "";
-    password = value;
-    if (value.length == 8) {
-      progressBar.classList.add("progressRed");
-    }
-    if (value.length > 9 && value.length < 12) {
-      progressBar.classList.replace("progressRed", "progressBlue");
-    }
-    if (value.length > 12) {
-      progressBar.classList.replace("progressBlue", "progressGreen");
-    }
+    return value
   }
 };
 
-const confPasswordCheck = (value) => {
-  if (value != password) {
-    errorDisplay("checkpassword", passwordError[1]);
+const confPasswordCheck = () => {
+  let value = confirmInp.value
+  if (value != passwordCheck()) {
+    createError(passwordError[1]);
     confirmPass = null;
   } else {
-    errorDisplay("checkpassword", "", true);
     confirmPass = value;
+    return true
   }
 };
 
 // Listeners
 
-inputs.forEach((input) => {
-  input.addEventListener("input", (e) => {
-    switch (e.target.id) {
-      case "pseudo":
-        pseudoCheck(e.target.value);
-        break;
-
-      case "mail":
-        emailCheck(e.target.value);
-        break;
-
-      case "password":
-        passwordCheck(e.target.value);
-        break;
-
-      case "checkpassword":
-        confPasswordCheck(e.target.value);
-        break;
-      default:
-        null;
-    }
-  });
-});
+passwordInp.addEventListener('input', e => {
+  if (e.target.value.length == 8) {
+    progressBar.className = "progressRed";
+  }
+  if (e.target.value.length > 9 && e.target.value.length < 12) {
+    progressBar.className = "progressBlue";
+  }
+  if (e.target.value.length > 12) {
+    progressBar.className = "progressGreen";
+  }
+})
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  if (pseudo && mail && password && confirmPass) {
-    const data = {
-      pseudo: pseudo,
-      mail: mail,
-      password: password,
-    };
-    users.push(data);
-    inputs.forEach((input) => {
-      input.value = null;
-      pseudo = null
-      mail = null
-      password = null
-      confirmPass = null
-      progressBar.classList.remove('progressRed', 'progressBlue', 'progressGreen')
-    });
-    alert("Votre compte est désormais créer !");
-  } else {
-    alert("Les champs du formulaire ne sont pas correctement renseignés.");
+  if (pseudoCheck()){
+    if (emailCheck()){
+      if (passwordCheck()){
+        if (confPasswordCheck()) {
+          const data = {
+            pseudo: pseudo,
+            mail: mail,
+            password: confirmPass,
+          };
+          users.push(data);
+          inputs.forEach((input) => {
+            input.value = null;
+            pseudo = null
+            mail = null
+            password = null
+            confirmPass = null
+            progressBar.classList.remove('progressRed', 'progressBlue', 'progressGreen')
+          });
+          createSuccess()
+        }
+      }
+    }
   }
-});
+})
+
